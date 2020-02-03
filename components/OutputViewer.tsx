333211ab6bb90b7455a5501c665276f5d2fbb26b
rhythm191 <sac2ndg+develop@gmail>
2020-02-03 14:31:53 +0900

    add: copy clipboard

--- components/OutputViewer.tsx
+++ components/OutputViewer.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo } from "react";
 import expand from "emmet";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 type Props = {
   text: string;
@@ -21,6 +22,27 @@ const OutputViewer: React.FC<Props> = ({ text }) => {
         <code>{formattedText}</code>
       </pre>
 
+      <CopyToClipboard text={formattedText}>
+        <button className="clipboard">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="far"
+            data-icon="clipboard"
+            className="svg-inline--fa fa-clipboard fa-w-12"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+          >
+            <path
+              fill="currentColor"
+              d="M336 64h-80c0-35.3-28.7-64-64-64s-64 28.7-64 64H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM192 40c13.3 0 24 10.7 24 24s-10.7 24-24 24-24-10.7-24-24 10.7-24 24-24zm144 418c0 3.3-2.7 6-6 6H54c-3.3 0-6-2.7-6-6V118c0-3.3 2.7-6 6-6h42v36c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-36h42c3.3 0 6 2.7 6 6z"
+            ></path>
+          </svg>
+          copy
+        </button>
+      </CopyToClipboard>
+
       <style jsx>{`
         .output-viewer {
           position: relative;
@@ -32,6 +54,20 @@ const OutputViewer: React.FC<Props> = ({ text }) => {
         .formatted-text {
           color: #333;
           font-size: 1.2rem;
+        }
+        .clipboard {
+          position: absolute;
+          top: 0;
+          right: 0;
+          color: #9a9a9a;
+          background-color: #f7f7f7;
+          cursor: pointer;
+        }
+        .clipboard > svg {
+          display: inline;
+        }
+        .clipboard > svg > path {
+          fill: #9a9a9a;
         }
       `}</style>
     </div>
